cec28574bcff3527e837f22f78a4c87606881e4d
Aayush Sachan <aayush.sachan.mat24@itbhu.ac.in>
2025-10-13 18:16:46 +0530

    added AI feedback functionality and get response in JSON format

--- app/components/fileuploader.tsx
+++ app/components/fileuploader.tsx
@@ -1,20 +1,40 @@
 
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
-
-export default function Fileuploader() {
-    const onDrop = useCallback((acceptedFiles:any) => {
-    // Do something with the files
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+interface prop{
+  onuploadfile:(file:File|null)=>void;
+}
+export default function Fileuploader({onuploadfile}:prop) {
+  const[file,setfile]=useState<File|null>(null);
+  
+    const onDrop = useCallback((acceptedFiles:File[]) => {
+      const newfile =  acceptedFiles[0]||null;
+      setfile(newfile)
+      if(newfile!=null)console.log(newfile.name)
+      onuploadfile(newfile);
+    }, [onuploadfile]);
+  const {getRootProps, getInputProps, isDragActive,isFocused,acceptedFiles} = useDropzone({onDrop,multiple:false,maxSize:20*1024*1024});
+  
+  
+  
   return (
-    <div className='gradient-border w-full cursor-pointer'>
+    <div className={`gradient-border w-full cursor-pointer `}>
         <div {...getRootProps()}>
       <input {...getInputProps()} />
       {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
+        (file==null) ?
+          (<div>
+            {isDragActive?<p>drop your files</p>:<p>Click or drag to upload file</p>}
+          </div>) :
+          (<div>
+            <img src="../public/images/pdf.png" alt="" className='size-10' />
+            <p>{`File name:${file.name} File size:${file.size}`}</p>
+            <button onClick={(e)=>{
+              e.stopPropagation();
+              onuploadfile(null);
+              setfile(null);
+            }} className='back-button'>remove</button>
+          </div>)
       }
     </div>
     </div>
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,6 +1,11 @@
 import React, { useState, type FormEvent } from 'react'
 import Navbar from '~/components/navbar'
 import Fileuploader from '~/components/fileuploader'
+import { usePuterStore } from '~/lib/puter'
+// import { prepareInstructions } from '~/constants/index'
+import { prepareInstructions } from '../../constants/index'
+import { AIResponseFormat } from '../../constants/index' // Update the path if 'prepareInstructions' is exported from '~/constants'
+import { useNavigate } from 'react-router'
 export const meta=()=>{
     return [
         {title:"upload"},
@@ -8,11 +13,74 @@ export const meta=()=>{
     ]
 }
 const upload = () => {
+    const {fs,kv,ai}=usePuterStore();
+    const [isprocessing,setisprocessing]=useState(false);
+    const [statustext,setstatustext]=useState("");
+    const navigate=useNavigate();
+    const[file,setfile]=useState<File|null>(null);
+    function onuploadfile(file:File|null){
+        setfile(file);
+        if(file!=null)console.log(file.size);
+        if(file==null)console.log("undefiend")
+    }
+    const handleanalyze=async (data:{ [k: string]: FormDataEntryValue})=>{
+        if(!file){
+            console.log("file nahi update hui")
+            return;
+        }
+        setisprocessing(true);
+        setstatustext("uploading your file");
+        
+        
+        const uploadedfile = await fs.upload([file]);
+        if(!uploadedfile){
+            setstatustext('Error cannot upload');
+            return ;
+        }
+        setstatustext('preparing your data');
+        const uuid = crypto.randomUUID();
+        const finaldata={
+            id:uuid,
+            resumepath:uploadedfile.path,
+            companyname : data.company_name,
+            jobtitle: data.Job_title,
+            jobdesc: data.job_description,
+            feedback:"",
+        }
+        kv.set(`resume:${uuid}`,JSON.stringify(data));
+        setstatustext("analyzing your data");
+        const jobTitle=data.Job_title as string;
+        const jobDescription=data.job_description as string;
+         const feedback = await ai.feedback(
+            uploadedfile.path,
+            prepareInstructions({ jobTitle, jobDescription,AIResponseFormat })
+        )
+        if(!feedback)return;
+        const feedbackText = typeof feedback.message.content === 'string'
+            ? feedback.message.content
+            : feedback.message.content[0].text;
+        
+        finaldata.feedback= JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setstatustext('Analysis complete, redirecting...');
+        console.log(finaldata.feedback);
+        setisprocessing(false);
 
-    const [isprocessing,setisprocessign]=useState(false);
-    const handlesubmit = (e:FormEvent<HTMLFormElement>)=>{
+        setstatustext("");
+        // navigate(`/resume/${uuid}`);
+
 
     }
+    const handlesubmit = (e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        const form=(e.currentTarget.closest('form'));
+        if(!form)return;
+        const formData = new FormData(form)
+        const data = Object.fromEntries(formData.entries());
+        
+        handleanalyze(data)
+    }
+    console.log(statustext);
   return (
     <main className="bg-[url('./images/bg-main.svg')] bg-cover">
         <Navbar/>
@@ -20,28 +88,31 @@ const upload = () => {
             <div className="page-heading">
                 <h1>Smart feedback for your dream job </h1>
                 {(isprocessing)?(
-                    <img src="/images/resume-scan.gif" alt="" className='w-full' />
+                    <div>
+                    <img src="../public/images/resume-scan.gif" alt="" className='w-full' />
+                    <h2>{statustext}</h2>
+                    </div>
                 ):(<h2>Drop your resume for an ATS score and improvement tips</h2>)}
                 {!isprocessing && 
                     <form onSubmit={handlesubmit} id='upload-form' className='flex flex-col gap-4 m-3'>
                         <div className='form-div' >
                             <label htmlFor="company_name">Company Name</label>
-                            <input type="text" name="company_name" id="company_name" placeholder='Company Name'/>
+                            <input type="text" name="company_name" id="company_name" placeholder='Company Name' />
                         </div>
                         <div className='form-div' >
                             <label htmlFor="Job_title">Job title</label>
-                            <input type="text" name="Job_title" id="Job_title" placeholder='Job title'/>
+                            <input type="text" name="Job_title" id="Job_title" placeholder='Job title' />
                         </div>
                         <div className='form-div' >
                             <label htmlFor="job_description">Job description</label>
-                            <textarea rows={4} name="job_description" id="job_description" placeholder='job description'/>
+                            <textarea rows={4} name="job_description" id="job_description" placeholder='job description' />
                         </div>
                         <div className='form-div' >
                             <label htmlFor="Document">Upload document</label>
-                            <Fileuploader/>
+                            <Fileuploader onuploadfile={onuploadfile}/>
                             
                         </div>
-                        <button className='primary-button' type='submit'>
+                        <button className="primary-button disabled:opacity-50 disabled:cursor-not-allowed" disabled={!file} type="submit" >
                             start analyzing
                         </button>
 
